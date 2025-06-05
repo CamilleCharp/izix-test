793874814eec3484c7c22c3bb6890fea6397d98a
Camille Zerrouk <camille.zerrouk@ccharp.dev>
2025-06-05 17:02:12 +0200

    Setup of the API + PostgreSQL environment

---
+++ api/docker/postgres/init.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -e
+
+# Set up the basic DB and its associated user on container creation
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<- EOSQL
+        CREATE USER $API_USER WITH ENCRYPTED PASSWORD '$API_PASSWORD';
+        CREATE DATABASE $API_DB;
+        GRANT ALL PRIVILEGES ON DATABASE $API_DB TO $API_USER;
+EOSQL
+
+# Needed to allow DB operations for the new user
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$API_DB" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO $API_USER;
+EOSQL
