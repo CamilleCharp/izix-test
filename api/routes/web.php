5f28d3c946d62ed20267e768aecc8c109a6bc04b
Camille Zerrouk <camille.zerrouk@ccharp.dev>
2025-06-06 11:13:42 +0200

    Post breeze API mode install state

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['message' => 'Welcome !']);
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
