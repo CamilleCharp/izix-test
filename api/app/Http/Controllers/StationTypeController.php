b99a9438205e9eb7eac1457d4517305109f4664d
Camille Zerrouk <camille.zerrouk@ccharp.dev>
2025-06-07 11:48:00 +0200

    Put StationTypeController in line with API structure

--- api/app/Http/Controllers/StationTypeController.php
+++ api/app/Http/Controllers/StationTypeController.php
@@ -2,29 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\Current;
-use App\Http\Requests\StationTypeRegisterRequest;
+use App\Enums\Permissions;
+use App\Http\Requests\StationTypeStoreRequest;
+use App\Http\Requests\StationTypeUpdateRequest;
 use App\Models\StationLevel;
 use App\Models\StationType;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Validation\Rule;
 
 class StationTypeController extends Controller
 {
-    public function register(StationTypeRegisterRequest $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $level = StationLevel::where('level', $request->input('level'))->first();
+        $stationTypes = StationType::all()->map->only(['name', 'level', 'current', 'power'])->toArray();
 
-        if(!$level) {
-            return response()->json(['message' => 'Invalid station level'], Response::HTTP_BAD_REQUEST);
-        }
+        return response()->json(['station_types' => $stationTypes]);
+    }
 
-        if($request->input('power') < $level->minimum_output || $request->input('power') > $level->maximum_output) {
-            return response()->json([
-                'message' => "Power output must be between {$level->minimum_output}W and {$level->maximum_output}W for level {$level->level}."
-            ], Response::HTTP_BAD_REQUEST);
-        }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StationTypeStoreRequest $request)
+    {
+        $this->getValidLevel($request->input('level'), $request->input('power'));
 
         $stationType = new StationType();
         $stationType->name = $request->input('name');
@@ -35,5 +38,73 @@ class StationTypeController extends Controller
         $stationType->save();
 
         return response(status: Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StationType $stationType)
+    {
+        return response()->json([
+            'station_type' => $stationType->only(['name', 'level', 'current', 'power'])
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StationTypeUpdateRequest $request, StationType $stationType)
+    {
+        
+        if($request->has('level') || $request->has('power')) {
+            $level = $request->input('level', $stationType->level);
+            $power = $request->input('power', $stationType->power);
+
+            $newLevel = $this->getValidLevel(
+                $request->input('level', $stationType->level),
+                $request->input('power', $stationType->power)
+            );
+            
+            if($level !== $stationType->level) {
+                $stationType->level()->associate($newLevel);
+            }
+        }
+
+        $stationType->name = $request->input('name', $stationType->name);
+        $stationType->current = $request->input('current', $stationType->current);
+        $stationType->power = $request->input('power', $stationType->power);
+
+        $stationType->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StationType $stationType)
+    {
+        if(!auth()->user()->hasPermissionTo(Permissions::DELETE_CHARGING_STATION)) {
+            return response()->json(['message' => 'You do not have permission to delete a charging station type.'], 403);
+        }
+        $name = $stationType->name;
+        $stationType->delete();
+
+        return response()->json(['message' => "Station type '{$name}' deleted successfully."], Response::HTTP_OK);
+    }
+
+    private function getValidLevel(int $level, int $power): JsonResponse|StationLevel
+    {
+        $level = StationLevel::where('level', $level)->first();
+
+        if(!$level) {
+            return response()->json(['message' => 'Invalid station level'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if($power < $level->minimum_output || $power > $level->maximum_output) {
+            return response()->json([
+                'message' => "Power output must be between {$level->minimum_output}W and {$level->maximum_output}W for level {$level->level}."
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        return $level;
     }
 }
---
+++ api/app/Http/Requests/StationTypeUpdateRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Current;
+use App\Enums\Permissions;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StationTypeUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->hasPermissionTo(Permissions::UPDATE_CHARGING_STATION_TYPE);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['string', 'max:255'],
+            'current' => [Rule::enum(Current::class)],
+            'level' => ['bail', 'integer', 'exists:station_levels,level'],
+            'power' => ['numeric'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.string' => 'The station type name must be a string.',
+            'name.max' => 'The station type name may not be greater than 255 characters.',
+            'current.enum' => 'The current type must be either AC or DC.',
+            'level.integer' => 'The station level must be an integer.',
+            'level.exists' => 'The selected station level does not exist.',
+            'power.numeric' => 'The power output must be a number.',
+        ];
+    }
+}
