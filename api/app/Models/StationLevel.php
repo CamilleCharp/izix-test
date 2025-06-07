3bd150cb63df696e892e5f49f0f3312feb093dbc
Camille Zerrouk <camille.zerrouk@ccharp.dev>
2025-06-07 08:06:35 +0200

    Added explicit relationship between station level and type

--- api/app/Models/StationLevel.php
+++ api/app/Models/StationLevel.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StationLevel extends Model
 {
     public $timestamps = false;
+
+    public function types(): HasMany {
+        return $this->hasMany(StationType::class);
+    }
 }
--- api/app/Models/StationType.php
+++ api/app/Models/StationType.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StationType extends Model
 {
-    //
+    protected $fillable = ['name', 'current', 'power'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(StationLevel::class, 'station_level_id');
+    }
 }
