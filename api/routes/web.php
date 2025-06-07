3da69ae6cf16860cc4649ef2fd29275449b12ec8
Camille Zerrouk <camille.zerrouk@ccharp.dev>
2025-06-07 11:51:03 +0200

    Added Station Types and Tenants CRUD route

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StationTypeController;
+use App\Http\Controllers\TenantController;
 use App\Http\Middleware\EnsureIsAdmin;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Models\Permission;
@@ -8,16 +9,37 @@ use Spatie\Permission\Models\Permission;
 Route::get('/', function () {
     return response()->json([
         'message' => 'Welcome to the API',
-        'documentation' => url('/docs'),
-        'user' => auth()->user(),
     ]);
 });
 
 Route::prefix('stations')->group(function() {
     Route::prefix('types')->group(function() {
-        Route::post('/register', [StationTypeController::class, 'register'])
+        Route::get('/', [StationTypeController::class, 'index']);
+        Route::get('/{stationType}', [StationTypeController::class, 'show']);
+
+        Route::post('/register', [StationTypeController::class, 'store'])
+            ->middleware(['auth:sanctum', EnsureIsAdmin::class]);
+
+        Route::put('/update/{stationType}', [StationTypeController::class, 'update'])
+            ->middleware(['auth:sanctum', EnsureIsAdmin::class]);
+
+        Route::delete('/delete/{stationType}', [StationTypeController::class, 'destroy'])
             ->middleware(['auth:sanctum', EnsureIsAdmin::class]);
     });
+});
+
+Route::prefix('tenants')->group(function() {
+    Route::get('/', [TenantController::class,'index']);
+    Route::get('/{tenant}', [TenantController::class, 'show']);
+
+    Route::post('/store/', [TenantController::class, 'store'])
+        ->middleware(['auth:sanctum', EnsureIsAdmin::class]);
+
+    Route::put('/update/{tenant}', [TenantController::class, 'update'])
+        ->middleware(['auth:sanctum', EnsureIsAdmin::class]);
+
+    Route::delete('/delete/{tenant}', [TenantController::class, 'destroy'])
+        ->middleware(['auth:sanctum', EnsureIsAdmin::class]);
 });
 
 require __DIR__.'/auth.php';
