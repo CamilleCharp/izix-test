92d129407cac017f95b98c67d144f3f6ff5fdca1
Camille Zerrouk <camille.zerrouk@ccharp.dev>
2025-06-07 09:12:52 +0200

    Added new roles

--- api/app/Enums/Roles.php
+++ api/app/Enums/Roles.php
@@ -7,13 +7,16 @@ enum Roles: string
     case ADMIN = 'admin';
     case USER = 'user';
     case GUEST = 'guest';
-
+    case STATION = 'station';
+    case VEHICLE = 'vehicle';
     public function label(): string
     {
         return match ($this) {
             self::ADMIN => 'Administrator',
             self::USER => 'User',
             self::GUEST => 'Guest',
+            self::STATION => 'Station',
+            self::VEHICLE => 'Vehicle',
         };
     }
 }
